665c6e1be3aa5ab2f1e80fd4d86f1a702e720d74
CreamR <2965750900@qq.com>
2022-12-17 16:42:50 +0800

    bug fix

--- src/components/table.vue
+++ src/components/table.vue
@@ -1,16 +1,19 @@
 <template>
 	<div class="table">
-		<ul
-			v-for="(item, index) in data.playlist"
-			:key="index"
-		>
-			<li></li>
+		<article></article>
+		<ul>
+			<li
+				v-for="(item, index) in data.playlist"
+				:key="index"
+			>
+				{{ item }}
+			</li>
 		</ul>
 	</div>
 </template>
 
 <script setup>
-	import { onMounted, reactive } from 'vue'
+	import { onMounted, reactive, watch } from 'vue'
 	import { getPlaylistDetail } from '../service/playlist'
 
 	const props = defineProps({
@@ -19,18 +22,26 @@
 	const data = reactive({ playlist: [] })
 
 	onMounted(() => {
-		init()
-	})
-	const init = async () => {
-		// 由于父组件playlist加载时，props并未传入，所以这里需要延迟加载并判断传来的props是否有效
-		setTimeout(async () => {
-			if (props.listID) {
-				const res = await getPlaylistDetail(props.listID)
-				data.playlist = res.songs
-				console.log(res)
-			}
+		setTimeout(() => {
+			init(props.listID)
 		}, 1024)
+	})
+	const init = async id => {
+		// 由于父组件playlist加载时，props并未传入，所以这里需要延迟加载并判断传来的props是否有效
+
+		console.log(id)
+		if (id) {
+			const res = await getPlaylistDetail(id)
+			data.playlist = res.songs
+			console.log(res)
+		}
 	}
+	watch(
+		() => props.listID,
+		newVal => {
+			init(newVal)
+		}
+	)
 </script>
 
 <style lang="less" scoped></style>
