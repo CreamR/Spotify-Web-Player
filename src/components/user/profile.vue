8bdaa45bc4f110379b7c0d897fd6aa6d890aac13
CreamR <2965750900@qq.com>
2023-02-16 19:20:55 +0800

    ⚡ 修复了用户资料初始化的bug

--- src/components/user/profile.vue
+++ src/components/user/profile.vue
@@ -1,11 +1,24 @@
 <template>
 	<div class="profile">
-		<h1 class="title">设置与个人信息</h1>
-		<span>昵称: {{ data.userName }}</span>
-		<span>uID: {{ data.userID }}</span>
+		<h1 class="title">用户资料</h1>
+		<p>昵称: {{ data.userData.name }}</p>
+		<p>听过歌曲数量: {{ data.userData.listenSongs }}</p>
+		性别：
+		<el-radio-group v-model="data.userData.gender">
+			<el-radio
+				:label="1"
+				size="large"
+				>男</el-radio
+			>
+			<el-radio
+				:label="0"
+				size="large"
+				>女</el-radio
+			>
+		</el-radio-group>
+
 		<el-button
 			type="danger"
-			dark="true"
 			@click="gologout"
 			>登出</el-button
 		>
@@ -13,25 +26,45 @@
 </template>
 
 <script setup>
-	import { reactive } from 'vue'
-	import { useRouter, useRoute } from 'vue-router'
-	import { logout } from '../../service/user'
+	import { reactive, onMounted } from 'vue'
+	import { useRouter } from 'vue-router'
+	import { logout, getProfile } from '../../service/user'
 
 	const router = useRouter()
-	const route = useRoute()
 
 	const data = reactive({
 		// 是否可以修改用户资料
 		type: false,
+		userData: {
+			name: '',
+			signature: '',
+			gender: 1,
+			listenSongs: 0,
+			photoUrl: '',
+		},
 	})
 
+	onMounted(() => {
+		init(localStorage.getItem('userID'))
+	})
+
+	const init = async uID => {
+		const res = await getProfile(uID)
+
+		data.userData.name = res.profile.nickname
+		data.userData.signature = res.profile.signature
+		data.userData.gender = res.profile.gender
+		data.userData.listenSongs = res.listenSongs
+		data.userData.photoUrl = res.profile.avatarUrl
+	}
+
+	// 退出登录
 	const gologout = async () => {
 		const res = await logout()
 		if (res.code == 200) {
 			localStorage.clear()
 			router.push({ name: 'login' })
 		} else {
-			// 退出登录失败
 			ElNotification({
 				title: '错误',
 				message: '退出登录失败',
@@ -43,5 +76,9 @@
 
 <style lang="less" scoped>
 	.profile {
+		.el-button {
+			margin-top: 200px;
+			display: block;
+		}
 	}
 </style>
